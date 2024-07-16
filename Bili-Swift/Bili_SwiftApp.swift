eba39b585652b40c3a472f12d3c1aa4a0ffcb574
zhihaofans <zhihaofans@hotmail.com>
2024-07-16 22:27:35 +0800

    Initial Commit

---
+++ Bili-Swift/Bili_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bili_SwiftApp.swift
+//  Bili-Swift
+//
+//  Created by zzh on 2024/7/16.
+//
+
+import SwiftUI
+
+@main
+struct Bili_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
