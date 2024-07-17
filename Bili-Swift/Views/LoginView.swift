86530547c346d34b630d27c40304ca82a291d056
zhihaofans <zhihaofans@hotmail.com>
2024-07-17 22:39:57 +0800

    Update LoginView

--- Bili-Swift/Views/LoginView.swift
+++ Bili-Swift/Views/LoginView.swift
@@ -23,65 +23,84 @@ struct iosLoginView: View {
     // Login data
     @State private var showingAlert = false
     @State private var alertText: String = "未知错误"
+    @State private var loadLoginDataFinished = false
     @State private var loginUrl: String = ""
     @State private var qrcodeKey: String = ""
     @State private var qrcodeText: String = "等待获取登录链接"
     private let loginService = LoginService()
     var body: some View {
         VStack {
-            NavigationView {
-                Text("打开哔哩哔哩扫描二维码登录")
-                    .font(.largeTitle)
-                    .padding()
-                if loginUrl.isEmpty {
-                    Text(qrcodeText)
-                } else {
-                    Button(action: {
-                        self.loginService.checkWebLoginQrcode(qrcodeKey: self.qrcodeKey) { checkResult in
-                            if checkResult.code == 0 {
-                                let setSu = KeychainUtil().saveString(forKey: "bilibili.login.refresh_token", value: checkResult.refresh_token)
-                                alertText = "保存登录数据" + setSu.string(trueStr: "成功", falseStr: "失败")
-                                showingAlert = true
-                            } else {
-                                alertText = checkResult.message
-                                showingAlert = true
-                            }
-                        } fail: { error in
-                            alertText = error
-                            showingAlert = true
-                        }
-
-                    }) {
-                        Text("我已经登录").font(.title)
-                    }
-                    .alert("Error", isPresented: $showingAlert) {
-                        Button("OK", action: {})
-                    } message: {
-                        Text(alertText)
-                    }
-                    .padding()
-                }
+            Button(action: {
+                print("loadLoginDataFinished:\(loadLoginDataFinished)")
+                print("loginUrl:\(loginUrl)")
+            }) {
+                Text("重新加载").font(.title)
             }
-            .onAppear {
-                if LoginService().isLogin() {
-                    qrcodeText = "已登录"
-                } else {
-                    self.loginService.getWebLoginQrcode { loginData in
-                        if !loginData.qrcode_key.isEmpty {
-                            qrcodeKey = loginData.qrcode_key
-                            // let qrcodeUrl = "https://passport.bilibili.com/h5-app/passport/login/scan?navhide=1&from=&qrcode_key=" + qrcodeKey
-                            // self.qrCodeImage = QrcodeUtil().generateQRCode(from: loginData.url)
+            .padding()
+            if !loadLoginDataFinished {
+                Text(qrcodeText)
+            } else {
+                Button(action: {
+                    Task {
+                        // 在这里执行耗时的任务
+                        let openSu = await AppService().openAppUrl(url: loginUrl)
+                        // 完成后，在主线程更新 UI
+                        DispatchQueue.main.async {
+                            // 更新 UI
+                            print("打开app：" + openSu.string(trueStr: "Su", falseStr: "fail"))
+                        }
+                    }
+                }) {
+                    Text("打开App登录").font(.title)
+                }
+                .padding()
+                Button(action: {
+                    self.loginService.checkWebLoginQrcode(qrcodeKey: self.qrcodeKey) { checkResult in
+                        if checkResult.code == 0 {
+                            let setSu = KeychainUtil().saveString(forKey: "bilibili.login.refresh_token", value: checkResult.refresh_token)
+                            alertText = "保存登录数据" + setSu.string(trueStr: "成功", falseStr: "失败")
+                            showingAlert = true
                         } else {
-                            alertText = "空白二维码"
+                            alertText = checkResult.message
                             showingAlert = true
                         }
-                    } fail: { errorMsg in
-
+                    } fail: { error in
+                        alertText = error
                         showingAlert = true
-                        alertText = errorMsg
                     }
-                    // self.getQrcodeData()
+
+                }) {
+                    Text("我已经登录").font(.title)
                 }
+                .alert("Error", isPresented: $showingAlert) {
+                    Button("OK", action: {})
+                } message: {
+                    Text(alertText)
+                }
+                .padding()
+            }
+
+        }.onAppear {
+            if LoginService().isLogin() {
+                qrcodeText = "已登录"
+            } else {
+                self.loginService.getWebLoginQrcode { loginData in
+                    print(loginData)
+                    if loginData.qrcode_key.isNotEmpty {
+                        qrcodeKey = loginData.qrcode_key
+                        loginUrl = loginData.url
+                        // self.qrCodeImage = QrcodeUtil().generateQRCode(from: loginData.url)
+                        loadLoginDataFinished = true
+                    } else {
+                        alertText = "空白二维码"
+                        showingAlert = true
+                    }
+                } fail: { errorMsg in
+
+                    showingAlert = true
+                    alertText = errorMsg
+                }
+                // self.getQrcodeData()
             }
         }.navigationTitle("登入")
     }
