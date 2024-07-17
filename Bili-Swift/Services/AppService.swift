37e80ee392cd895f2e50d6a61fd16daea9d0691f
zhihaofans <zhihaofans@hotmail.com>
2024-07-17 22:47:14 +0800

    Update AppService

--- Bili-Swift/Services/AppService.swift
+++ Bili-Swift/Services/AppService.swift
@@ -9,11 +9,21 @@ import Foundation
 import SwiftUtils
 
 class AppService {
+    func getBiliUrl(url: String) -> String {
+        return "bilibili://browser/?url=\(url.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed) ?? ""))"
+    }
+
     func openAppUrl(url: String) async -> Bool {
-        return await AppUtil().openUrl(url: <#T##URL#>)
+        let biliUrl = self.getBiliUrl(url: url)
+        print(biliUrl)
+        return await AppUtil().openUrl(urlString: url)
     }
 
     func isAppIntalled() -> Bool {
+        #if os(iOS)
         return AppUtil().canOpenUrl(urlString: "bilibili://")
+        #else
+        return true
+        #endif
     }
 }
