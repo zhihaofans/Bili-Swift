936db79f7cd857b72d3a5003a99add737abc8f07
zhihaofans <zhihaofans@hotmail.com>
2024-07-18 22:09:55 +0800

    Update HistoryView

--- Bili-Swift/Views/HistoryView.swift
+++ Bili-Swift/Views/HistoryView.swift
@@ -13,11 +13,11 @@ struct HistoryView: View {
     @State var errorStr=""
     @State var historyList: [HistoryItem]=[]
     var body: some View {
-        VStack {
+        ScrollView {
             if loaded {
                 if isError {
                     Text(errorStr).font(.largeTitle)
-                }else{
+                } else {
                     LazyVStack {
                         ForEach(historyList, id: \.history.oid) { item in
                             HistoryItemView(itemData: item)
@@ -27,20 +27,33 @@ struct HistoryView: View {
             } else {
                 Text("Loading...")
             }
-        }.onAppear {
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Image(systemName: "trash")
+            }
+        }
+        .navigationTitle("历史记录")
+        .onAppear {
             // TODO: 加载历史数据
-            HistoryService().getHistory { result in
-                if result.data.list.isEmpty {
-                    isError=true
-                    errorStr="空白结果列表"
-                } else {
-                    historyList=result.data.list
-                    isError=false
+            Task {
+                HistoryService().getHistory { result in
+                    DispatchQueue.main.async {
+                        if result.data.list.isEmpty {
+                            isError=true
+                            errorStr="空白结果列表"
+                        } else {
+                            historyList=result.data.list
+                            isError=false
+                        }
+                        loaded=true
+                    }
+                } fail: { err in
+                    DispatchQueue.main.async {
+                        isError=true
+                        errorStr=err
+                    }
                 }
-                loaded=true
-            } fail: { err in
-                isError=true
-                errorStr=err
             }
         }
     }
@@ -49,7 +62,7 @@ struct HistoryView: View {
 struct HistoryItemView: View {
     var itemData: HistoryItem
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
             HStack {
                 AsyncImage(url: URL(string: itemData.getCover())) { image in
                     image
@@ -60,11 +73,11 @@ struct HistoryItemView: View {
                 }
                 .frame(width: 150, height: 84)
                 VStack {
-                    Text(itemData.title).font(.largeTitle)
-                    Text(itemData.author_name)
+                    Text(itemData.title) // .font()
+                    Text("@" + itemData.author_name)
                 }
                 // .frame(width: geometry.size.width)
-            }
+            }.frame(maxHeight: .infinity, alignment: .leading) // 设置对齐方式
         }
         .frame(height: 100) // 将 VStack 的固定高度设置为100
         .contentShape(Rectangle()) // 加这行才实现可点击
