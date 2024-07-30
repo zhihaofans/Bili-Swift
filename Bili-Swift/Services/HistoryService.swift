1cd2ee76509f289c3e40ad767cf02a56f10cff7c
zhihaofans <zhihaofans@hotmail.com>
2024-07-31 00:50:36 +0800

    Update

--- Bili-Swift/Services/HistoryService.swift
+++ Bili-Swift/Services/HistoryService.swift
@@ -58,7 +58,7 @@ class HistoryService {
             } else {
                 print(result)
                 do {
-                    let data = try JSONDecoder().decode(VipCheckinResult.self, from: result.data(using: .utf8)!)
+                    let data = try JSONDecoder().decode(HistoryResult.self, from: result.data(using: .utf8)!)
                     debugPrint(data.code)
                     if data.code == 0 {
                         callback(data)
