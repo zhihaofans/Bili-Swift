fa559b4e69a2340170bf13efaca4e686e5851b1a
zhihaofans <zhihaofans@hotmail.com>
2024-10-06 09:05:38 +0800

    update

--- Bili-Swift/Models/DynamicModel.swift
+++ Bili-Swift/Models/DynamicModel.swift
@@ -37,8 +37,8 @@ struct DynamicListItemBasic: Codable {
 }
 
 struct DynamicListItemModules: Codable {
-    let module_author: DynamicListItemBModuleAuthor
-    //   let module_dynamic    obj    动态内容信息
+    let module_author: DynamicListItemModuleAuthor
+    let module_dynamic: DynamicListItemModuleDynamic
     //  let module_more    obj    动态右上角三点菜单
     //  let  module_stat    obj    动态统计数据
 //    module_interaction    obj    热度评论
@@ -47,7 +47,7 @@ struct DynamicListItemModules: Codable {
 //    module_tag    obj    置顶信息
 }
 
-struct DynamicListItemBModuleAuthor: Codable {
+struct DynamicListItemModuleAuthor: Codable {
     let face: String
     let jump_url: String
     let label: String // 名称前标签：合集、电视剧、番剧
@@ -56,4 +56,30 @@ struct DynamicListItemBModuleAuthor: Codable {
     let pub_action: String // 更新动作描述
     let pub_time: String // 更新时间：x分钟前、x小时前、昨天
     let pub_ts: Int // 更新时间戳    UNIX 秒级时间戳
+}
+
+struct DynamicListItemModuleDynamic: Codable {
+    let additional: String? // 相关内容卡片信息
+    let desc: String? // 动态文字内容,其他动态时为null
+    let major: DynamicListItemModuleDynamicMajor? // 动态主体对象,转发动态时为null
+    let topic: String? // 话题信息
+}
+
+struct DynamicListItemModuleDynamicMajor: Codable {
+    let type: String // 动态主体类型
+    let archive: DynamicListItemModuleDynamicMajorArchive? // type=MAJOR_TYPE_ARCHIVE
+}
+
+struct DynamicListItemModuleDynamicMajorArchive: Codable {
+    let aid: String // 视频AV号
+//    badge    obj    角标信息
+    let bvid: String // 视频BVID
+    let cover: String // 视频封面
+    let desc: String // 视频简介
+    disable_preview num 0
+    duration_text str 视频长度
+    jump_url str 跳转URL
+    stat obj 统计信息
+    title str 视频标题
+//    type    num    1
 }
