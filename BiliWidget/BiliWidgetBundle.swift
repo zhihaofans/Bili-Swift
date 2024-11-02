7e409100b8f35a12f21e7a8daee51befade13f4f
zhihaofans <zhihaofans@hotmail.com>
2024-11-03 00:31:31 +0800

    小组件:开始搞

---
+++ BiliWidget/BiliWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BiliWidgetBundle.swift
+//  BiliWidget
+//
+//  Created by zzh on 2024/11/2.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BiliWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BiliWidget()
+    }
+}
